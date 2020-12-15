8dc488d14319441d5dc5960c66160a384a7912f7
pikokr <parangee9706@gmail.com>
2020-12-15 21:27:34 +0900

    fix: change dropdown button style

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -13,7 +13,25 @@
           class="popover-button"
           @click.stop="userPopoverOpen = !userPopoverOpen"
         >
-          {{ $store.state.user.username }}#{{ $store.state.user.discriminator }}
+          <img
+            :src="
+              $store.state.user.avatar
+                ? `https://cdn.discordapp.com/avatars/${$store.state.user.id}/${$store.state.user.avatar}`
+                : 'https://cdn.discordapp.com/embed/avatars/' +
+                  $store.state.user.discriminator
+            "
+            alt="avatar"
+            class="avatar"
+          />
+          <span class="popover-button__username">{{
+            $store.state.user.username
+          }}</span>
+          <div
+            :class="{
+              'popover-button__arrow': true,
+              active: userPopoverOpen,
+            }"
+          />
         </div>
         <div :class="{ 'popover-container': true, show: userPopoverOpen }">
           POPOVER
@@ -64,5 +82,33 @@ export default {
 .popover-button {
   cursor: pointer;
   user-select: none;
+  display: flex;
+  align-items: center;
+}
+.avatar {
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+  border-radius: 50%;
+}
+.popover-button__arrow {
+  border: solid var(--text-primary);
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+  margin-left: 10px;
+  transform: rotate(45deg);
+  transition: transform 0.2s ease-in-out;
+  &.active {
+    transform: rotate(225deg);
+  }
+}
+.popover-button__username {
+  @media (min-width: 769px) {
+    display: inline-block;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
 }
 </style>
